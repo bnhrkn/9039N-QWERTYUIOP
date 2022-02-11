5ced94c9e14d394b6a2412de540ee4b73fe9cacd
bnhrkn <github@harkin.tk>
2022-02-11 18:00:24 -0500

    Pre-competition chassis gyro code, display auton selector, misc edits 	modified:   src/display.hpp 	modified:   src/main.cpp

--- src/display.hpp
+++ src/display.hpp
@@ -3,12 +3,18 @@
 namespace display {
 
 static const char * homeBtnMatrixMap[] = {"Auton Selection", "Pre-flight Setup", "\n",
-					"Tests", "Graphs", "Calibration", "\n", ""};
+					"Tests", "Graphs", "Calibration", ""};
 lv_obj_t* homeBtnMatrix = lv_btnm_create(lv_scr_act(), NULL);
 
 lv_obj_t* autonSelectorScr = lv_obj_create(NULL, NULL);
 lv_obj_t* graphsScr = lv_obj_create(NULL, NULL);
 lv_obj_t* calibrateScr = lv_obj_create(NULL, NULL);
+
+static const char * autonBtnMatrixMap[] = {"Dual HG AWP L.", "\n",
+											"Fast Neutral & Single HG AWP L.", "\n",
+											"Fast Neutral & Single HG AWP R.", "Disable", ""}; 
+lv_obj_t* autonBtnMatrix = lv_btnm_create(autonSelectorScr, NULL);
+lv_obj_t* autonSelectedLbl = lv_label_create(autonSelectorScr, NULL);
 
 auto defaultScr = lv_scr_act();
 /*
@@ -37,9 +43,8 @@ void autonSelectorBtnAction() {
 	
 }
 void preRunBtnAction() {
-	liftControl->setTarget(-17.75);
 	liftControl->flipDisable(false);
-	
+	liftControl->setTarget(-17.75);
 }
 
 void graphsBtnAction() {
@@ -65,7 +70,7 @@ lv_res_t homeBtnAction(lv_obj_t* btn) {
 lv_res_t homeBtnMatrixHandler(lv_obj_t* obj, const char* txt) {
 	std::string istr = txt;
 	std::map<std::string, int> map { {"Auton Selection", 0}, {"Pre-flight Setup", 1}, {"Tests", 2}, {"Graphs", 3}, {"Calibration", 4}};
-	switch(map.at(txt)) {
+	switch(map.at(istr)) {
 		case 0:
 			autonSelectorBtnAction();
 			break;
@@ -86,6 +91,16 @@ lv_res_t homeBtnMatrixHandler(lv_obj_t* obj, const char* txt) {
 	return LV_RES_OK;
 }
 
+lv_res_t autonBtnMatrixHandler(lv_obj_t* obj, const char* txt) {
+	std::map<std::string, int> map { {"Dual HG AWP L.", 0 }, 
+									{ "Fast Neutral & Single HG AWP L.", 1 }, 
+									{ "Fast Neutral & Single HG AWP R.", 2}, 
+									{ "Disable", 3 } };
+	autonMode = map.at(txt);
+	lv_label_set_text(autonSelectedLbl, txt );
+	return LV_RES_OK;
+}
+
 void startScreen() {
 	lv_theme_t* th = lv_theme_night_init(266, &lv_font_dejavu_20);
 	lv_theme_set_current(th);
@@ -95,10 +110,20 @@ void startScreen() {
 	lv_obj_set_size(homeBtnMatrix, LV_HOR_RES - 10, 115);
 	lv_obj_align(homeBtnMatrix, NULL, LV_ALIGN_IN_TOP_MID, 0, 5);
 	
+	lv_btnm_set_map(autonBtnMatrix, autonBtnMatrixMap);
+	lv_btnm_set_action(autonBtnMatrix, autonBtnMatrixHandler);
+	lv_obj_set_size(autonBtnMatrix, LV_HOR_RES - 10, LV_VER_RES - 80);
+	lv_obj_align(autonBtnMatrix, NULL, LV_ALIGN_IN_TOP_MID, 0, 45);
 	
-	lv_obj_set_size(homeBtn, 55, 55);
+	lv_obj_set_size(autonSelectedLbl, LV_HOR_RES - 10, 35);
+	lv_obj_set_pos(autonSelectedLbl, 5, 5);
+	lv_label_set_text(autonSelectedLbl, "Selected: Disabled");
+	lv_label_set_align(autonSelectedLbl, LV_LABEL_ALIGN_LEFT);
+
+	lv_obj_set_size(homeBtn, 35, 35);
 	lv_obj_set_hidden(homeBtn, true);
-	lv_obj_set_pos(homeBtn, 420, 181);
+	lv_obj_align(homeBtn, NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 5, 5);
+	//lv_obj_set_pos(homeBtn, 420, 181);
 	lv_label_set_text(homeBtnLbl, SYMBOL_HOME);
 	lv_btn_set_action(homeBtn, LV_BTN_ACTION_CLICK, homeBtnAction);
 /*
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,18 @@
 #include <string>
 #include "../include/okapi/impl/util/configurableTimeUtilFactory.hpp"
 #include <map>
+#include <limits>
 
 using namespace okapi::literals;
+
+auto logger = std::make_shared<okapi::Logger> (
+            okapi::TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
+            "/ser/sout", // Output to the PROS terminal
+            okapi::Logger::LogLevel::debug // Show errors and warnings
+        );
+
+//Construct the IMU
+pros::Imu inertial(4);
 
 class cXDriveModel : public okapi::XDriveModel {
   public:
@@ -53,9 +63,162 @@ class cRotationSensor : public okapi::RotationSensor {
 			}
 		}
 };
+/*
+class cIterativePosPIDController : public okapi::IterativePosPIDController {
+	using okapi::IterativePosPIDController::IterativePosPIDController;
+	public:
+		double step(const double inewReading) {
+		  if (controllerIsDisabled) {
+			return 0;
+		  } else {
+			loopDtTimer->placeHardMark();
 
-//Construct the IMU
-pros::Imu inertial(4);
+			if (loopDtTimer->getDtFromHardMark() >= sampleTime) {
+			  // lastReading must only be updated here so its updates are time-gated by sampleTime
+			  const double readingDiff = inertial.get_heading() - lastReading;
+			  lastReading = inertial.get_heading();
+				std::cout << "Reading: " << readingDiff;
+			  error = getError();
+				std::cout << "Error: " << error;
+			  if ((std::abs(error) < target - errorSumMin && std::abs(error) > target - errorSumMax) ||
+				  (std::abs(error) > target + errorSumMin && std::abs(error) < target + errorSumMax)) {
+				integral += kI * error; // Eliminate integral kick while realtime tuning
+			  }
+
+			  if (shouldResetOnCross && std::copysign(1.0, error) != std::copysign(1.0, lastError)) {
+				integral = 0;
+			  }
+
+			  integral = std::clamp(integral, integralMin, integralMax);
+
+			  // Derivative over measurement to eliminate derivative kick on setpoint change
+			  derivative = derivativeFilter->filter(readingDiff);
+
+			  output = std::clamp(kP * error + integral - kD * derivative + kBias, outputMin, outputMax);
+
+			  lastError = error;
+			  loopDtTimer->clearHardMark(); // Important that we only clear if dt >= sampleTime
+
+			  settledUtil->isSettled(error);
+			}
+		  }
+
+		  return output;
+	} 
+};
+*/
+class cChassisControllerPID : public okapi::ChassisControllerPID {
+protected:
+	const pros::Imu& Imu;
+public:
+//	using okapi::ChassisControllerPID::ChassisControllerPID;
+	cChassisControllerPID(
+			okapi::TimeUtil itimeUtil,
+			std::shared_ptr<okapi::ChassisModel> ichassisModel,
+			std::unique_ptr<okapi::IterativePosPIDController> idistanceController,
+			std::unique_ptr<okapi::IterativePosPIDController> iturnController,
+			std::unique_ptr<okapi::IterativePosPIDController> iangleController,
+			const okapi::AbstractMotor::GearsetRatioPair &igearset,
+			const okapi::ChassisScales &iscales,
+			std::shared_ptr<okapi::Logger> ilogger,
+			const pros::Imu& iIMU)
+			: okapi::ChassisControllerPID(
+				itimeUtil,
+				ichassisModel,
+				std::move(idistanceController),
+				std::move(iturnController),
+				std::move(iangleController),
+				igearset,
+				iscales,
+				ilogger
+			),
+			Imu(iIMU) {}
+	~cChassisControllerPID() {
+		dtorCalled.store(true, std::memory_order_release);
+		delete task;
+	}
+	void loop() {
+		LOG_INFO_S("Started ChassisControllerPID task.");
+
+  auto encStartVals = chassisModel->getSensorVals();
+  std::valarray<std::int32_t> encVals;
+  double distanceElapsed = 0, angleChange = 0;
+  modeType pastMode = none;
+  auto rate = timeUtil.getRate();
+
+  while (!dtorCalled.load(std::memory_order_acquire) && !task->notifyTake(0)) {
+    /**
+     * doneLooping is set to false by moveDistanceAsync and turnAngleAsync and then set to true by
+     * waitUntilSettled
+     */
+    if (doneLooping.load(std::memory_order_acquire)) {
+      doneLoopingSeen.store(true, std::memory_order_release);
+    } else {
+      if (mode != pastMode || newMovement.load(std::memory_order_acquire)) {
+        encStartVals = chassisModel->getSensorVals();
+        newMovement.store(false, std::memory_order_release);
+      }
+
+      switch (mode) {
+      case distance:
+        encVals = chassisModel->getSensorVals() - encStartVals;
+        distanceElapsed = static_cast<double>((encVals[0] + encVals[1])) / 2.0;
+        //angleChange = static_cast<double>(encVals[0] - encVals[1]);
+		angleChange = Imu.get_rotation() * scales.turn;
+        distancePid->step(distanceElapsed);
+        anglePid->step(angleChange - turnPid->getTarget());
+
+        if (velocityMode) {
+          chassisModel->driveVector(distancePid->getOutput(), anglePid->getOutput());
+        } else {
+          chassisModel->driveVectorVoltage(distancePid->getOutput(), anglePid->getOutput());
+        }
+
+        break;
+
+      case angle:
+        encVals = chassisModel->getSensorVals() - encStartVals;
+//        angleChange = (encVals[0] - encVals[1]) / 2.0;
+		angleChange = Imu.get_rotation() * scales.turn;
+		turnPid->step(angleChange);
+
+        if (velocityMode) {
+          chassisModel->driveVector(0, turnPid->getOutput());
+        } else {
+          chassisModel->driveVectorVoltage(0, turnPid->getOutput());
+        }
+
+        break;
+
+      default:
+        break;
+      }
+
+      pastMode = mode;
+    }
+
+    rate->delayUntil(threadSleepTime);
+  }
+
+  stop();
+
+  LOG_INFO_S("Stopped ChassisControllerPID task.");
+}
+void startThread() {
+	if (!task) {
+    task = new CrossplatformThread(trampoline, this, "ChassisControllerPID");
+  }
+}
+static void trampoline(void *context) {
+  if (context) {
+    static_cast<cChassisControllerPID *>(context)->loop();
+  }
+}
+
+
+};
+
+
 
 // Construct the rotation sensor
 auto rotationSensor { std::make_shared<cRotationSensor>(1, false) };
@@ -72,10 +235,53 @@ std::shared_ptr<okapi::AsyncPositionController<double, double>> liftControl =
 	.withSensor(baseRotarySensor)
 	.withGains({liftkP, liftkI, liftkD})
 	.withGearset(okapi::AbstractMotor::GearsetRatioPair(okapi::AbstractMotor::gearset::red, 1))
-	.withTimeUtilFactory(okapi::ConfigurableTimeUtilFactory(1, 0.5, 50_ms))
+	.withTimeUtilFactory(okapi::ConfigurableTimeUtilFactory(1, std::numeric_limits<double>::max(), 0_ms))
+	.withLogger(logger)
 	.build();
 
-// Construct the drivetrain
+// Drive Motors
+auto topLeft { std::make_shared<okapi::Motor>(2) };
+auto topRight { std::make_shared<okapi::Motor>(-7) };
+auto bottomRight { std::make_shared<okapi::Motor>(-11) };
+auto bottomLeft { std::make_shared<okapi::Motor>(20) };
+
+// Drivemodel
+auto XDriveTrain { std::make_shared<okapi::XDriveModel>(
+		topLeft,
+		topRight,
+		bottomRight,
+		bottomLeft,
+		topLeft->getEncoder(),
+		topRight->getEncoder(),
+		200,
+		12000) };
+
+// Chassis Controller PID Controllers
+//auto chassisTimeUtilFactory = okapi::ConfigurableTimeUtilFactory(1, 0.5, 50_ms);
+auto chassisTimeUtilFactory = okapi::TimeUtilFactory();
+
+// Construct the Chassis Controller
+auto drive { std::make_shared<cChassisControllerPID>(
+		chassisTimeUtilFactory.create(),
+		XDriveTrain,
+		std::make_unique<okapi::IterativePosPIDController>( //Distance
+            okapi::IterativePosPIDController::Gains{0.0019,0.0,0.00002}, // 0.0018 0.00018 0.0002
+            chassisTimeUtilFactory.create()
+        ),
+		std::make_unique<okapi::IterativePosPIDController>( //Turn
+            okapi::IterativePosPIDController::Gains{0.0013, 0.00015,0.0000},
+            chassisTimeUtilFactory.create()
+        ),
+		std::make_unique<okapi::IterativePosPIDController>( //Angle
+            okapi::IterativePosPIDController::Gains{0.0013,0.00015,0.0000},
+            chassisTimeUtilFactory.create()
+        ),
+		okapi::AbstractMotor::gearset::green,
+		okapi::ChassisScales({4_in,15.5_in}, okapi::imev5GreenTPR),
+		logger,
+		inertial
+		)};
+/*
 std::shared_ptr<okapi::ChassisController> drive = 
 	okapi::ChassisControllerBuilder()
 		.withMotors(
@@ -86,7 +292,8 @@ std::shared_ptr<okapi::ChassisController> drive =
 			   )
 		.withDimensions(okapi::AbstractMotor::gearset::green, {{4_in, 15.5_in}, okapi::imev5GreenTPR})
 		.build();
-
+*/
+/*
 std::shared_ptr<okapi::AsyncMotionProfileController> profiler = 
 	okapi::AsyncMotionProfileControllerBuilder()
 		.withLimits({
@@ -96,9 +303,9 @@ std::shared_ptr<okapi::AsyncMotionProfileController> profiler =
 			})
 		.withOutput(drive)
 		.buildMotionProfileController();
-
+*/
 //auto XDriveTrain { std::static_pointer_cast<okapi::XDriveModel>(drive->getModel()) };
-auto cXDriveTrain { std::static_pointer_cast<cXDriveModel>(drive->getModel()) };
+auto cXDriveTrain { std::static_pointer_cast<cXDriveModel>(XDriveTrain) };
 //auto XDriveTrain { std::dynamic_pointer_cast<cXDriveModel>(drive->getModel()) };
 //auto XDriveTrain { std::
 auto liftPIDController { std::dynamic_pointer_cast<okapi::AsyncPosPIDController>(liftControl) };
@@ -136,17 +343,18 @@ lv_obj_t* calibrateBtn;
 lv_obj_t* calibrateBtnLbl;
 */
  
+int autonMode { 3 }; // Autonomous Mode Variable set by selector
 #include "display.hpp"
 
 void initialize() {
-	
+	drive->startThread();
 	display::startScreen();	
 
 	okapi::Logger::setDefaultLogger (
 		std::make_shared<okapi::Logger> (
 			okapi::TimeUtilFactory::createDefault().getTimer(), // It needs a Timer
 			"/ser/sout", // Output to the PROS terminal
-			okapi::Logger::LogLevel::debug // Show errors and warnings
+			okapi::Logger::LogLevel::info // Show errors and warnings
 		)
 	);
 	liftGroup.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
@@ -162,52 +370,58 @@ void disabled() {
 void competition_initialize() {
 }
 
+
 void autonomous() {
-	liftControl->flipDisable(false);
-	liftControl->setTarget(95);
-	liftControl->waitUntilSettled();
-	controller.rumble("-");
-	/*
-//	liftControl->setTarget(95);
-//	pros::delay(500);
-//	drive->moveDistance(3_ft);
-//	liftControl->setTarget(55);
-//	pros::delay(500);
-//	drive->moveDistance(-2_ft);
-	//cXDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
-	liftControl->flipDisable(false);
-	liftControl->setTarget(95);
-	pros::delay(750);
-	cXDriveTrain->forward(200);
-	pros::delay(1100);
-	cXDriveTrain->stop();
-	pros::delay(250);
-	liftControl->setTarget(55);
-	pros::delay(500);
-	
-	//strafe start
+	switch(autonMode) {
+		case 0: // Dual home goal wp
+			liftControl->flipDisable(false);
+			liftControl->setTarget(95);
+				pros::delay(50);
+				liftControl->waitUntilSettled();
+			drive->moveDistance(15_in);
+			liftControl->setTarget(55);
+			cXDriveTrain->strafe(150);
+			pros::delay(1350);
+			cXDriveTrain->stop();
+			drive->turnAngle(0_deg);
+			liftControl->setTarget(95);
+			pros::delay(250);
+			drive->moveDistance(100_in);
+			liftControl->setTarget(55);
+				pros::delay(50);
+				liftControl->waitUntilSettled();
+			cXDriveTrain->forward(-200);
+			pros::delay(1000);
+			cXDriveTrain->stop();
+		case 1: // Left Side Fast Neutral and score hg
+			liftControl->flipDisable(false);
+			liftControl->setTarget(95);
+				//pros::delay(50);
+				//liftControl->waitUntilSettled();
+			drive->moveDistance(51_in);
+			liftControl->setTarget(55);
+			pros::delay(200);
+			drive->moveDistance(-51_in);
+			drive->turnAngle(90_deg);
+			liftControl->setTarget(95);
+				pros::delay(50);
+				liftControl->waitUntilSettled();
+			drive->moveDistance(15_in);
+			liftControl->setTarget(55);
+				pros::delay(50);
+				liftControl->waitUntilSettled();
+			cXDriveTrain->forward(-200);
+			pros::delay(500);
+			cXDriveTrain->stop();
+			drive->turnAngle(45_deg);
+			drive->moveDistance(2_ft);
+			break;
+		case 2: // Right Side Fast neutral and score hg
+			break;
+		//default:
+			//Do nothing
 
-	pros::delay(250);
-
-	cXDriveTrain->strafe(-150);
-	pros::delay(1000);
-	cXDriveTrain->stop();
-	pros::delay(250);
-
-	//strafe end
-
-	cXDriveTrain->forward(-150);
-	cXDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
-	pros::delay(1400); // straight reverse delay
-//	pros::delay(800); // strafe reverse delay
-	cXDriveTrain->stop();
-//strafe turn
-	cXDriveTrain->rotate(-200);
-	pros::delay(350);
-	cXDriveTrain->stop();
-// strafe end turn
-	liftControl->setTarget(0);
-*/	
+	}
 }
 
 void opcontrol() {
@@ -240,19 +454,27 @@ void opcontrol() {
 
 		//Forks
 //		lift.move_velocity(1.5625 * driveLut[master.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y)]);
-		if (eStop.changedToPressed()) {
-
+		if (eStop.isPressed()) {
+			drive->turnAngle(0_deg);
+			drive->moveDistance(6_ft);
+			//drive->turnAngle(90_deg);
+			
+//		std::cout << inertial.get_heading() << "\n";
+/*
 		std::cout << 
 			"target: " << liftControl->getTarget() << 
 			" error: " << liftControl->getError() <<
 			" position: " << rotationSensor->get() <<
+			" settled: " << liftControl->isSettled() <<
 			//" cposition: " << crotationSensor->get() <<
 			"\n";
-		/*	while(eStop.isPressed()) {
-				liftGroup.moveVoltage(controller.getAnalog(okapi::ControllerAnalog::rightY)	);
-				pros::delay(5);
-			}*/
-		}
+			//while(eStop.isPressed()) {
+			//	liftGroup.moveVoltage(controller.getAnalog(okapi::ControllerAnalog::rightY)	);
+			//	pros::delay(5);
+			//}*/
+		} 
+
+	//}
 //		static double prevRightY;
 		double rightY = controller.getAnalog(okapi::ControllerAnalog::rightY);
 
@@ -285,73 +507,75 @@ void opcontrol() {
 		//Tilt Lock	
 		static bool tilted { false };
 		int roll = std::abs(inertial.get_roll());
-		if (!tilted && roll >= 10 && roll < 1000) {
+		if (!tilted && roll >= 3 && roll < 1000) {
 			std::cout << "triggered tiltbrake\n";
 			cXDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
 			tilted = !tilted;
 		}
-		else if (tilted && roll < 10) {
-			cXDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::coast);
+		else if (tilted && roll < 3) {
+			cXDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::brake);
 			tilted = !tilted;
 		}
 
+		static double ckP { 0.0000 };
+		static double ckI { 0.0000 };
+		static double ckD { 0.0000 };
 
 		// PID Tuning
-//		static int pidsel { 0 };
-//		if (buttonY.changedToPressed()) {
-//			pidsel = 0;
-//		}
-//		if (buttonB.changedToPressed()) {
-//			pidsel = 1;
-//		}
-//		if (buttonA.changedToPressed()) {
-//			pidsel = 2;
-//		}
-//		if (pidsel == 0) {
-//			if (buttonUp.changedToPressed()){
-//				liftkP = liftkP + 0.001;
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				controller.setText(0, 0, std::to_string( liftkP ));
-//			}
-//			if (buttonDown.changedToPressed()){
-//				liftkP = liftkP - 0.001;
-//				controller.setText(0, 0, std::to_string( liftkP ));
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//			}
-//		}
-//
-//		if (pidsel == 1) {
-//			if (buttonUp.changedToPressed()){
-//				liftkI = liftkI + 0.0001;
-//				controller.setText(0, 0, std::to_string( liftkI ));
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//			}
-//			if (buttonDown.changedToPressed()){
-//				liftkI = liftkI - 0.0001;
-//				controller.setText(0, 0, std::to_string( liftkI ));
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//			}
-//		}
-//
-//		if (pidsel == 2) {
-//			if (buttonUp.changedToPressed()){
-//				liftkD = liftkD + 0.0001;
-//				controller.setText(0, 0, std::to_string( liftkD ));
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//			}
-//			if (buttonDown.changedToPressed()){
-//				liftkD = liftkD - 0.0001;
-//				controller.setText(0, 0, std::to_string( liftkD ));
-//				std::cout << "gains: " << liftkP << ", " << liftkI << ", " << liftkD << "\n";
-//				liftPIDController->setGains({liftkP, liftkI, liftkD});
-//			}
-//		}
+		static int pidsel { 0 };
+		if (buttonY.changedToPressed()) {
+			pidsel = 0;
+		}
+		if (buttonB.changedToPressed()) {
+			pidsel = 1;
+		}
+		if (buttonA.changedToPressed()) {
+			pidsel = 2;
+		}
+		if (pidsel == 0) {
+			if (buttonUp.changedToPressed()){
+				ckP = ckP + 0.0001;
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+				controller.setText(0, 0, std::to_string( ckP ));
+			}
+			if (buttonDown.changedToPressed()){
+				ckP = ckP - 0.0001;
+				controller.setText(0, 0, std::to_string( ckP ));
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+			}
+		}
 
+		if (pidsel == 1) {
+			if (buttonUp.changedToPressed()){
+				ckI = ckI + 0.00001;
+				controller.setText(0, 0, std::to_string( ckI ));
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+			}
+			if (buttonDown.changedToPressed()){
+				ckI = ckI - 0.00001;
+				controller.setText(0, 0, std::to_string( ckI ));
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+			}
+		}
+
+		if (pidsel == 2) {
+			if (buttonUp.changedToPressed()){
+				ckD = ckD + 0.00001;
+				controller.setText(0, 0, std::to_string( ckD ));
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+			}
+			if (buttonDown.changedToPressed()){
+				ckD = ckD - 0.00001;
+				controller.setText(0, 0, std::to_string( ckD ));
+				std::cout << "gains: " << ckP << ", " << ckI << ", " << ckD << "\n";
+			}
+		}
+
+/*			drive->setGains(
+					okapi::IterativePosPIDController::Gains{ckP,ckI,ckD}, 
+					okapi::IterativePosPIDController::Gains{0.0013, 0.00015,0.00001}, 
+					okapi::IterativePosPIDController::Gains{0.0013,0.00015,0.00001});
+	*/				
 //		static bool toggle { false };
 //		if (holdButton.changedToPressed()) {
 //			toggle = !toggle;
@@ -362,7 +586,6 @@ void opcontrol() {
 //				XDriveTrain->setBrakeMode(okapi::AbstractMotor::brakeMode::coast);
 //			}
 //		}
-
 		pros::delay(10);
 	}
 }
